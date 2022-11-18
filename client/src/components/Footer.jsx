f44a27cc8ba733e3d691a693594f2e1371439b13
Patrick Tung <83930287+Patrickrtu@users.noreply.github.com>
2022-11-18 11:12:21 -0700

    add Footer.jsx

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+// Copy this file to Footer.jsx and replace it with your information. It will
+// not be commited to GitHub.
+const Footer = () => {
+  return (
+    <footer>
+      <h4>Hosted by: Patrick Tung ~ k44b938</h4>
+      <p>
+        {" "}
+        <b>Port:</b> 30/5047{" "}
+      </p>
+      <p> </p>
+    </footer>
+  );
+};
+
+export default Footer;
