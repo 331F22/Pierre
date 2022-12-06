f45035c1fb4b4f0cd3bf82dfd8685f61bfeacf79
Bennett Sampson <sampson246156@gmail.com>
2022-12-06 14:57:17 -0700

    why

--- client/src/components/CurrentEntries.jsx
+++ client/src/components/CurrentEntries.jsx
@@ -207,7 +207,7 @@ const CurrentEntries = () => {
                 }
               }}>Update</button>
 
-              <button className='update' style= {{buttonStyle}}onClick={() => {
+              <button className='update' onClick={() => {
                 if (newEmail.length > 0) {
                   updateEmail(val.email_address);
                 }
