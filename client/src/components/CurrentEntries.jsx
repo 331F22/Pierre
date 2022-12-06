397f14db20005716073815c2a664619cbd1d8778
Bennett Sampson <sampson246156@gmail.com>
2022-12-06 14:53:17 -0700

    Isabel is being mean

--- client/src/components/CurrentEntries.jsx
+++ client/src/components/CurrentEntries.jsx
@@ -180,6 +180,10 @@ const CurrentEntries = () => {
       editPasscodeInput.style.visibility = 'hidden'
     }
   }
+  // var buttonStyle = {
+  //   color: 'white',
+  //   backgroundColor = ''
+  // }
 
   return (
 
@@ -202,7 +206,7 @@ const CurrentEntries = () => {
                 }
               }}>Update</button>
 
-              <button className='ban' style= {{backgroundColor:  rgb(175, 20, 20), color: white, display:'none'}}onClick={() => {
+              <button className='ban' style= {{backgroundColor:  'rgb(175, 20, 20)', color: 'white', display:'none'}}onClick={() => {
                 if (newEmail.length > 0) {
                   updateEmail(val.email_address);
                 }
