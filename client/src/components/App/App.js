c7b4af90310e1b4898e6864e99ffff6cb1022d80
user-nate <70123268+user-nate@users.noreply.github.com>
2022-12-09 14:27:07 -0700

    Update App.js

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
 import AddEntry from '../AddEntry.jsx';
+import Header from '../Header.jsx';
+import Waiver from '../Waiver.jsx';
 import CurrentEntries from '../CurrentEntries.jsx';
+
 import Footer from '../Footer.jsx'
 
 function App() {
 
   return (
     <div className="App">
+	<Header/>
+	
       <h1>Entries</h1>
-
-      <AddEntry />
       
+      <AddEntry />
       <hr />
       <CurrentEntries />
       <hr />
@@ -22,4 +26,6 @@ function App() {
 }
 
 export default App;
+
+
 
