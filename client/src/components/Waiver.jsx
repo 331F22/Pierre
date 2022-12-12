233283ca2be22d937a8fbe02bfa65b5b9924763e
user-nate <70123268+user-nate@users.noreply.github.com>
2022-12-12 00:19:05 -0700

    Create Waiver.jsx

---
+++ client/src/components/Waiver.jsx
@@ -0,0 +1,15 @@
+import { react } from 'react';                                                                                                                                                                                                                  
+const Waiver = () => {                                                                                                          
+return (                                                                                                                        
+<div className="Waiver">                                                                                                
+	<h1> Waiver </h1>                                                                                                       
+		<img src="https://images.sampleforms.com/wp-content/uploads/2017/05/Generic-Waiver-Release-Form.jpg" />                 
+			<p>                                                                                                                             
+				By clicking the switch below you are agreeing to sign the waiver that you read above. Bridger Ski Foundation 
+				is not responisble for and injuries you may sustain durning your services with Bridge ski foundation.                                                                                                                            
+			</p>                                                                                                                    
+	</div>                                                                                                          
+	)
+     	}                                                                                                                       
+
+export default Waiver; 
