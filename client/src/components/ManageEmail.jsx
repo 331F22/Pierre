4d7c144a60d952685f9725d02ea202db73781ccb
lgood163 <77400565+lgood163@users.noreply.github.com>
2022-12-12 21:50:20 -0700

    Edit code back to original

--- client/src/components/ManageEmail.jsx
+++ client/src/components/ManageEmail.jsx
@@ -1,58 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './ManageEmail.css';
 
-const ManageEmail = () => {
+const ManageEmail = (props) => {
 
   return ( 
-
-    <div>
-      <h2>New Email</h2>
-      <label>Send to:</label>
-      <div>
-        <input id="customEmail" type="text" name="email" />
-        <button className="OptionBtn">All</button>
-        <button className="OptionBtn">Scheduled Volunteers</button>
-      </div>
-      <label>Subject:</label>
-      <div>
-        <input id="customSubject" type="text" name="subject" />
+    const [button1,setButton1] = useState(false);
+    const [button2,setButton2] = useState(false);
+    const [recipientBox, setRecipientBox] = useState("");
+  
+    const handleButton1Click = () => {
+        setButton1(true);
+        setButton2(false);
+        setRecipientBox("");
+      }
+    
+    const handleButton2Click = () => {
+        setButton1(false);
+        setButton2(true);
+        setRecipientBox("");
+      }
+    
+    const [button3,setButton3] = useState(false);
+    const [button4,setButton4] = useState(false);
+    const [button5,setButton5] = useState(false);
+    const [subjectBox, setSubjectBox] = useState("");
+    const [bodyBox, setBodyBox] = useState("");
+    
+    const handleButton3Click = () => {
+        setButton3(true);
+        setButton4(false);
+        setButton5(false);
+        setSubjectBox("");
+        setBodyBox("");
+      }
+    
+    const handleButton4Click = () => {
+        setButton3(false);
+        setButton4(true);
+        setButton5(false);
+        setSubjectBox("");
+        setBodyBox("");
+      }
+    
+    const handleButton5Click = () => {
+        setButton3(false);
+        setButton4(false);
+        setButton5(true);
+      }
+    
+    return ( 
+        <div>
+        <h2>New Email</h2>
+        <label>Send to:</label>
+        <div>
+            <input className="emailInput" type="text" value={recipientBox} onChange={e => {
+                setRecipientBox(e.target.value)
+                setButton1(false) 
+                setButton2(false)
+            }}/>   
+            <button onClick={handleButton1Click} className={button1 ? 'on' : 'off'}>All</button>
+            <button onClick={handleButton2Click} className={button2 ? 'on' : 'off'}>Scheduled Volunteers</button>
+        </div>
+        <label>Subject:</label>
+        <div>   
+            <input className="emailInput" type="text" value={subjectBox} onChange={e => {
+                setSubjectBox(e.target.value)
+                setButton3(false) 
+                setButton4(false)
+                setButton5(false)
+            }}/> 
       </div>
       <label>Body:</label>
       <div>
-        <button className="OptionBtn">Voucher Template</button>
-        <button className="OptionBtn">Cancellation Template</button>
-        <button className="OptionBtn">Waiver Template</button>
-      </div>
-      <label>Custom:</label>
-      <div>
-        <input id="customBody" type="text" name="body" />
-      </div>
-      <div>
+            <button onClick={handleButton3Click} className={button3 ? 'on' : 'off'}>Voucher Template</button>
+            <button onClick={handleButton4Click} className={button4 ? 'on' : 'off'}>Cancellation Template</button>
+            <button onClick={handleButton5Click} className={button5 ? 'on' : 'off'}>Waiver Template</button>
+    </div>
+    <label>Custom:</label>
+    <div>  
+        <input className="emailInput" type="text" value={bodyBox} onChange={e => {
+            setBodyBox(e.target.value)
+            setButton3(false) 
+            setButton4(false)
+            setButton5(false)
+        }}/>
+    </div>
+    <div>
         <button className="clearBtn"
-        onClick={() => {
-          const confirmBox = window.confirm(
-            "Are you sure you want to clear all fields?"
-          )
-          if(confirmBox === true) {
-            alert("fields cleared")
+            onClick={() => {
+                const confirmBox = window.confirm(
+                "Are you sure you want to clear all fields?"
+            );
+
+          if (confirmBox === true) {
+            setRecipientBox("");
+            setSubjectBox("");
+            setBodyBox("");
+            setButton1(false);
+            setButton2(false);
+            setButton3(false);
+            setButton4(false);
+            setButton5(false);
           }
-        }}
-        >Clear</button>
-        <button className="sendBtn"                         // send button
-          onClick={() => {
-            const confirmBox = window.confirm(
-              "Are you sure you want to send this email?"
-            )
-            if (confirmBox === true) {
-
-            /*
-
-              For each comment below, the following logic determines who the email is sent to, as well as the subject and body of the
-              email. Use the comments as a guide to understand what each button means and extend this functionality to add the email
-              data to the backend database. From there, the data can be used to send an email using whatever API is running.
-
-            */
-
-              if (button1 === true) {
+    } }
+    >Clear</button>
+    <button className="sendBtn"
+      onClick={() => {
+        const confirmBox = window.confirm(
+          "Are you sure you want to send this email?"
+          ); 
+          if (confirmBox === true) {
+            if (button1 === true) {
                 /* 
                 ADD BACKEND FUNCTIONALITY
                 Recipient: select ALL entries in the database to send this email to 
@@ -115,8 +173,8 @@ const ManageEmail = () => {
             }
           } }
         >Send Email</button>
-      </div>
-    </div>
-  )
+         </div>
+</div>
+)
 }
 export default ManageEmail;
